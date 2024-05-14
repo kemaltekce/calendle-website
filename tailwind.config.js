1a38eec6be234ed319c225d92923c5cea6d14e18
kemal <kemal.tekce@gmail.com>
2024-05-14 10:11:09 +0200

    change design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './pages/**/*.{html,js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        rotate45: {
+          '0%, 100%': { transform: 'rotate(45deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        bullets: resolve(__dirname, 'pages/bullets.html'),
+        design: resolve(__dirname, 'pages/design.html'),
+        hotkeys: resolve(__dirname, 'pages/hotkeys.html'),
+        lists: resolve(__dirname, 'pages/lists.html'),
+        mode: resolve(__dirname, 'pages/mode.html'),
+        privacy: resolve(__dirname, 'pages/privacy.html'),
+        timer: resolve(__dirname, 'pages/timer.html'),
+      },
+    },
+  },
   plugins: [],
   css: {
     postcss: {
